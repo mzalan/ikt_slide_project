2217ae6b2d48cd8ede63773a60a6ae255aafddba
kecske5000 <marton.kristof@students.jedlik.eu>
2023-02-22 11:15:22 +0100

    upgrade gomb done

--- menu.py
+++ menu.py
@@ -14,18 +14,27 @@ def Menu(screen):
     title_text = r.gover_font.render("Slicky Slide", True, "black")
     title_rect = title_text.get_rect(center=(960,240))
 
-   
-    
+
+    upgrade_text = r.score_font.render("Upgrade", True, "black")
+    upgrade_rect = upgrade_text.get_rect(center=(960,640))
+
 
     exit_text = r.score_font.render("Exit", True, "black")
-    exit_rect = exit_text.get_rect(center=(960,640))
-    
+    exit_rect = exit_text.get_rect(center=(960,1040))
 
-    play_draw_surf = pygame.Surface((300,120))
+
+    play_draw_surf = pygame.Surface((350,120))
     play_draw_rect = play_draw_surf.get_rect(center=(960,440))
 
-    exit_draw_surf = pygame.Surface((300,120))
-    exit_draw_rect = exit_draw_surf.get_rect(center=(960,640))
+
+    upgrade_draw_surf = pygame.Surface((350,120))
+    upgrade_draw_rect = upgrade_draw_surf.get_rect(center=(960,640))
+
+
+    exit_draw_surf = pygame.Surface((350,120))
+    exit_draw_rect = exit_draw_surf.get_rect(center=(960,840))
+
+    
 
 #play gomb
 
@@ -47,6 +56,27 @@ def Menu(screen):
         play_text = r.score_font.render("Play", True, "white")
         play_rect = play_text.get_rect(center=(960,440))
     
+
+#upgrade gomb
+
+    if upgrade_draw_rect.x <= mouse[0] <= upgrade_draw_rect.x+upgrade_draw_rect.w and upgrade_draw_rect.y <= mouse[1] <= upgrade_draw_rect.y+upgrade_draw_rect.height:
+        drawing = pygame.draw.rect(upgrade_draw_surf, color_light, upgrade_draw_rect)
+        
+        upgrade_draw_surf.fill("#f5eeda")
+        screen.blit(upgrade_draw_surf, drawing)
+
+        upgrade_text = r.score_font.render("Upgrade", True, "black")
+        upgrade_rect = upgrade_text.get_rect(center=(960,640))
+        if r.click:
+            StartGame()
+    else:
+       
+        drawing = pygame.draw.rect(upgrade_draw_surf, "red",upgrade_draw_rect)
+        screen.blit(upgrade_draw_surf, drawing)
+
+        upgrade_text = r.score_font.render("Upgrade", True, "white")
+        upgrade_rect = upgrade_text.get_rect(center=(960,640))
+
 #exit gomb
 
     if exit_draw_rect.x <= mouse[0] <= exit_draw_rect.x+exit_draw_rect.w and exit_draw_rect.y <= mouse[1] <= exit_draw_rect.y+exit_draw_rect.height:
@@ -56,7 +86,7 @@ def Menu(screen):
         screen.blit(exit_draw_surf, drawing)
 
         exit_text = r.score_font.render("Exit", True, "black")
-        exit_rect = exit_text.get_rect(center=(960,640))
+        exit_rect = exit_text.get_rect(center=(960,840))
         if r.click:
             StartGame()
     else:
@@ -65,9 +95,11 @@ def Menu(screen):
         screen.blit(exit_draw_surf, drawing)
 
         exit_text = r.score_font.render("Exit", True, "white")
-        exit_rect = exit_text.get_rect(center=(960,640))
+        exit_rect = exit_text.get_rect(center=(960,840))
+
 
 
     r.screen.blit(title_text, title_rect)
     r.screen.blit(play_text, play_rect)
+    r.screen.blit(upgrade_text, upgrade_rect)
     r.screen.blit(exit_text, exit_rect)
